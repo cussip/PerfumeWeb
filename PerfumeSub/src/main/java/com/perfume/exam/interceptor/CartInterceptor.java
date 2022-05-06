5b09b60e655d48b8004c34dc412801f772feac47
ilho Noh <shdlfgh63@naver.com>
2022-05-06 15:42:42 +0900

    no message

--- PerfumeSub/src/main/java/com/perfume/exam/interceptor/CartInterceptor.java
+++ PerfumeSub/src/main/java/com/perfume/exam/interceptor/CartInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.perfume.exam.model.MemberVO;
 
+
 public class CartInterceptor implements HandlerInterceptor {
 
 	@Override
@@ -17,11 +18,13 @@ public class CartInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 		
 		MemberVO mvo = (MemberVO)session.getAttribute("member");
-		
+		System.out.println("전");
 		if(mvo == null) {
-			response.sendRedirect("/perfume/home");
-			return false;
+			 response.sendRedirect(request.getContextPath()+"/perfume/home");
+			 System.out.println("들어감");
+			 return false;
 		} else {
+			System.out.println("else");
 			return true;
 		}
 	}
