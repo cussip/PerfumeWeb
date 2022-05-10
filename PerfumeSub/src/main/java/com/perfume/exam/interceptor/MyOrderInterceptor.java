3af6e574363bb6bc81e6536f2c078c162ef8944a
Yoon-Heo <hyoon618@gmail.com>
2022-05-10 21:05:02 +0900

    [-- 21:03] myorder 테이블에 id 컬럼 추가 및 인터셉터를 통한 페이지 로그인 제한 반영

---
+++ PerfumeSub/src/main/java/com/perfume/exam/interceptor/MyOrderInterceptor.java
@@ -0,0 +1,31 @@
+package com.perfume.exam.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.perfume.exam.model.MemberVO;
+import com.perfume.exam.vo.MyOrderVO;
+
+public class MyOrderInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+	
+	HttpSession session	= request.getSession();
+	
+	MemberVO mvo = (MemberVO)session.getAttribute("member");
+	
+	if(mvo == null) {
+		response.sendRedirect("/perfume/home");
+		return false;
+	} else {
+		return true;
+	}
+	
+	}
+
+}
--- PerfumeSub/src/main/java/com/perfume/exam/service/dao/MyOrderDAO.java
+++ PerfumeSub/src/main/java/com/perfume/exam/service/dao/MyOrderDAO.java
@@ -6,7 +6,9 @@ import java.util.List;
 import com.perfume.exam.vo.MyOrderVO;
 
 public interface MyOrderDAO {
-	 List<MyOrderVO> selectOrderList() throws Exception;
+	
+	 // 주문내역 목록 (로그인 후 이용가능)
+	 List<MyOrderVO> selectOrderList(String id) throws Exception;
 	
 	 // 주문내역 삭제
 	 public void deleteOrder(int orderId) throws Exception;
