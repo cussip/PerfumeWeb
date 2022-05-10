d128c54907d8e99049c9b400d949f47ff01966cd
cjs3971 <js3971@gmail.com>
2022-05-10 19:04:37 +0900

    interceptor 기능 구현 비로그인시 장바구니 버튼 실행시 경고창 발생후 메인페이지로 이동
    header.jsp 로그인/로그아웃 기능 추가(테스트용)

--- PerfumeSub/src/main/java/com/perfume/exam/interceptor/CartInterceptor.java
+++ PerfumeSub/src/main/java/com/perfume/exam/interceptor/CartInterceptor.java
@@ -1,5 +1,7 @@
 package com.perfume.exam.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,11 +19,22 @@ public class CartInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 		
 		MemberVO mvo = (MemberVO)session.getAttribute("member");
-		
+		System.out.println("interceptor 진입");
 		if(mvo == null) {
-			response.sendRedirect("/perfume/home");
+			
+			System.out.println("interceptor 비로그인 상태");
+			
+			response.setContentType("text/html; charset=UTF-8"); 
+			PrintWriter out = response.getWriter();
+			 
+			out.println("<script>alert('장바구니는 로그인 후 사용 가능합니다'); location.href='/perfume/home'; </script>");
+			out.flush();
+						
 			return false;
 		} else {
+			
+			System.out.println("interceptor 로그인 상태");
+			
 			return true;
 		}
 	}
